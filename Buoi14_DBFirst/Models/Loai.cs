e1409f6fa95d7f3c555c32d936ee1a2e7e3cd09e
khanhphat <nguyenhuukhanhphat@gmail.com>
2019-06-23 11:40:53 +0700

    Up load DataBaseFirst

---
+++ Buoi14_DBFirst/Models/Loai.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Buoi14_DBFirst.Models
+{
+    public partial class Loai
+    {
+        public Loai()
+        {
+            HangHoa = new HashSet<HangHoa>();
+        }
+
+        public int MaLoai { get; set; }
+        public string TenLoai { get; set; }
+        public string MoTa { get; set; }
+        public string Hinh { get; set; }
+
+        public ICollection<HangHoa> HangHoa { get; set; }
+    }
+}
